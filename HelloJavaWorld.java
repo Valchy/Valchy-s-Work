84688907b9ca5e52285a966042c93d911cff0e11
Valchy <valerithekiller@gmail.com>
2018-01-18 18:06:33 +0200

    Update
    - Java bug fixed

--- HelloJavaWorld.java
+++ HelloJavaWorld.java
@@ -2,8 +2,8 @@
 import java.util.Scanner;
 public class HelloJavaWorld {
 	public static void main (String [] args) {
-		System.out.println('Hello Java World');
-		String stringVariable = 'My name is Valeri';
+		System.out.println("Hello Java World");
+		String stringVariable = "My name is Valeri";
 		int integerVariable = 3;
 		double decimalPointsVariable = 4.342;
 		float floatVariable = 4.5f;
@@ -14,7 +14,7 @@ public class HelloJavaWorld {
 		boolean booleanResult = randomInteger == 8;
 		System.out.println(booleanResult);
 
-		// Simple Operations: +, -, /, *, ++, --, % or Relational Operations: ==, !=, <=, <, >=, > or Logical Operators: && and/or ||
+		// // Simple Operations: +, -, /, *, ++, --, % or Relational Operations: ==, !=, <=, <, >=, > or Logical Operators: && and/or ||
 		int value = 10;
 		int numberOperations = 4;
 		value = value +1;
@@ -24,15 +24,17 @@ public class HelloJavaWorld {
 
 		numberOperations++;
 		numberOperations = 5;
+		boolean temp = integerVariable == 3 || floatVariable == 3f;
+		boolean tmp = integerVariable == 3 && floatVariable == 3f;
 
-		System.out.println('Random stuff: ' + (value*numberOperations));
-		System.out.println(boolean temp = integerVariable == 3 || floatVariable == 3f); // returns true
-		System.out.println(boolean tmp = integerVariable == 3 && floatVariable == 3f); // returns false
+		System.out.println("Random stuff: " + (value * numberOperations));
+		System.out.println(temp); // returns true
+		System.out.println(tmp); // returns false
 
 		int min = 1;
 		int max = 10;
 		int storeResult = ((int) (Math.random() * ((max - min) +1))) + min; // returns a number between 1 - 5;
-		System.out.prinln('Random generated number = '+storeResult);
+		System.out.println("Random generated number = " + storeResult);
 
 		// Name of Scanner variable is input
 		Scanner input = new Scanner(System.in);
