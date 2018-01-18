b725a1cf4c6bd59566ebba9d089a01a06773b5f2
Valchy <valerithekiller@gmail.com>
2018-01-18 14:47:32 +0200

    Update
    - Java stuff

--- HelloJavaWorld.java
+++ HelloJavaWorld.java
@@ -1,3 +1,5 @@
+// This imports a library to read users input
+import java.util.Scanner;
 public class HelloJavaWorld {
 	public static void main (String [] args) {
 		System.out.println('Hello Java World');
@@ -12,7 +14,7 @@ public class HelloJavaWorld {
 		boolean booleanResult = randomInteger == 8;
 		System.out.println(booleanResult);
 
-		// Simple Operations: +, -, /, *
+		// Simple Operations: +, -, /, *, ++, --, % or Relational Operations: ==, !=, <=, <, >=, > or Logical Operators: && and/or ||
 		int value = 10;
 		int numberOperations = 4;
 		value = value +1;
@@ -24,6 +26,27 @@ public class HelloJavaWorld {
 		numberOperations = 5;
 
 		System.out.println('Random stuff: ' + (value*numberOperations));
+		System.out.println(boolean temp = integerVariable == 3 || floatVariable == 3f); // returns true
+		System.out.println(boolean tmp = integerVariable == 3 && floatVariable == 3f); // returns false
+
+		int min = 1;
+		int max = 10;
+		int storeResult = ((int) (Math.random() * ((max - min) +1))) + min; // returns a number between 1 - 5;
+		System.out.prinln('Random generated number = '+storeResult);
+
+		// Name of Scanner variable is input
+		Scanner input = new Scanner(System.in);
+		int userNumber = 0;
+		userNumber = input.nextInt();
+		if (userNumber == storeResult) {
+			System.out.println("User Number and Random Number are the same!");
+		}
+		else if (userNumber == (storeResult+1) || userNumber == (storeResult-1)) {
+			System.out.println("User number is 1 more or less than the random number");
+		}
+		else {
+			System.out.println("Numbers to not match, try again :D");
+		}
 	}
 }
 
