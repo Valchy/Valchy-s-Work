101af6f0bfba1cba86e252cd3788d1fed6d6e204
Valchy <valerithekiller@gmail.com>
2018-03-22 13:53:05 +0200

    Update

--- customNodeModule.js
+++ customNodeModule.js
@@ -1,5 +1,5 @@
 // I this document I am creating a node.js module
-exports.customModule = function () {
+module.exports.customModule = function () { // For some reason this prints only as text...
 	var theDate = new Date();
 	theDate = 'Todays date: '+theDate;
 	return theDate;
